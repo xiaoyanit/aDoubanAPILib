088c72972099ed9cf9d4fb8d2893f8047e0f3451
Void Main <voidmain1313113@gmail.com>
2012-10-17 21:45:35 +0800

    create new project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Douban API wrapper for Android Library
---
+++ src/me/voidmain/lib/doubanapi/connector/BaseConnector.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package me.voidmain.lib.doubanapi.connector;
+
+/**
+ * Douban is composed of a set of different APIs, including music, books, movies and so on.
+ * Here, we need to provide a bunch of different classes for different APIS.
+ * 
+ * @author voidmain
+ *
+ */
+public abstract class BaseConnector {
+
+}
